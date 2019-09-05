81a5b0b10f8b77a141f57241524816ba4b669dce
boxer <boxers@126.com>
2019-09-05 12:55:54 +0800

    add django_rest_framework

--- projects/serializers.py
+++ projects/serializers.py
@@ -5,4 +5,4 @@ from .models import Project
 class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
-        fields = ('project_number', 'machinery_type')
+        fields = ('project_number', 'machinery_type', 'included_module')
