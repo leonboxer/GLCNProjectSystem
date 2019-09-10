3fef734a9d7ee9aec43e466be5e03c8b113b34fc
boxer <boxers@126.com>
2019-09-10 11:45:13 +0800

    add brands

--- brands/admin.py
+++ brands/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Brand
+
 
 # Register your models here.
+class BrandAdmin(admin.ModelAdmin):
+    list_display = [
+        'brand_name', 'description', 'created_time', 'update_time'
+    ]
+    list_filter = ['created_time', 'update_time']
+
+
+admin.site.register(Brand, BrandAdmin)
