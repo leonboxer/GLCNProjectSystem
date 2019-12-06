919294059a9ad59f6bad14f77c6b131befb09624
boxer <boxers@126.com>
2019-12-06 11:55:37 +0800

    override save_instance

--- tags/admin.py
+++ tags/admin.py
@@ -1,24 +1,39 @@
 from django.contrib import admin
 from .models import Tag
 from import_export.admin import ImportExportModelAdmin
-from import_export import resources
+from import_export import fields, resources
+from django.db import IntegrityError
 
 
 class TagResource(resources.ModelResource):
+    description = fields.Field(
+        column_name='Description EN',
+        attribute='description',
+    )
+    tag_number = fields.Field(
+        column_name='Tag No.',
+        attribute='tag_number'
+    )
+
+    def save_instance(self, instance, using_transactions=True, dry_run=False):
+        try:
+            super(TagResource, self).save_instance(instance, using_transactions, dry_run)
+        except IntegrityError:
+            pass
+
     class Meta:
         model = Tag
-        exclude = ('id',)
-        import_id_fields = ['tag_number']
-        # fields = ('brand')
+        skip_unchanged = True
+        report_skipped = True
+        fields = ('tag_number', 'created_time', 'description')
+        import_id_fields = ('tag_number',)
 
-
-# Register your models here.
 
 class TagAdmin(ImportExportModelAdmin):
-    resource_class = Tag
+    resource_class = TagResource
     list_display = ['tag_number', 'description', 'created_time', 'update_time']
     list_filter = ['created_time', 'update_time']
-    search_fields = ['tag_number']
+    search_fields = ['tag_number', 'description', 'created_time', 'update_time']
 
 
 admin.site.register(Tag, TagAdmin)
