729d0812c4ef772d94609da7ee717588e24395b1
boxer <boxers@126.com>
2020-01-18 15:30:04 +0800

    working when result:[1]

--- react_frontend/src/App.js
+++ react_frontend/src/App.js
@@ -6,42 +6,40 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            result:null
+            result: [1]
         };
-        this.fetchResult = this.fetchResult.bind(this);
         this.setResult = this.setResult.bind(this);
     }
 
-    fetchResult() {
-        fetch(
-            // '127.0.0.1:8000/projects/'
-                `https://hn.algolia.com/api/v1/search?query=redux`
+    componentDidMount() {
+        axios(
+            'http://127.0.0.1:8000/projects/'
+            // 'localhost/users'
+            // `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`
+            // 'https://hn.algolia.com/api/v1/search?query=redux'
         )
-            .then(response => response.json())
-            .then(result => this.setResult(result))
-            .catch(error => error)
+        // .then(response => response.json())
+            .then(result => this.setResult(result.data))
+            .then(error => error)
     }
 
     setResult(result) {
-        // const {project_number, machinery_type, created, updated} = result;
-        this.setState({
-            result:result
-        });
-    }
-
-    componentDidMount() {
-        this.fetchResult();
+        this.setState({result})
     }
 
 
     render() {
-        console.log('HEREEEE');
-        console.log(this.state.result);
+        console.log('result:',this.state.result);
         return (
-            this.state.result.hits.map(item =>
+            this.state.result.map(item =>
+               <div>
                 <div>
-                    {item}
+                    {item.project_number}
                 </div>
+                <div>
+                {item.created}
+                </div>
+               </div>
             )
         );
     }
